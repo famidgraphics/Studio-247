a45bed7e9150fdc486de968c2ba70e200e3ca39c
Famid Hossain <96484801+famidgraphics@users.noreply.github.com>
2023-01-04 13:05:43 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin:10,
+        items:7,
+        loop:true,
+        // nav:true,
+        autoplay:true,
+        dots:true,
+        
+
+    });
+
+
+
+  });
